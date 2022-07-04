cce27019d67e4ec700d1b37e0477ae002bca54ad
juankll2 <juanc.lazol@ucuenca.edu.ec>
2022-07-04 07:54:59 -0500

    Eureka server

--- registry-service/src/test/java/academy/digitallab/store/registry/RegistryServiceApplicationTests.java
+++ registry-service/src/test/java/academy/digitallab/store/registry/RegistryServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package academy.digitallab.store.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RegistryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
