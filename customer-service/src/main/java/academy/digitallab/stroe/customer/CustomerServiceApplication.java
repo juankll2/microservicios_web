f43c89c9ca5a652a5e1d892fc2cbe8ee3194e4fa
juankll2 <juanc.lazol@ucuenca.edu.ec>
2022-07-03 19:55:53 -0500

    archivos prueba

--- customer-service/src/main/java/academy/digitallab/stroe/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/academy/digitallab/stroe/customer/CustomerServiceApplication.java
@@ -3,11 +3,17 @@ package academy.digitallab.stroe.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
+
+		SpringApplication app = new SpringApplication(CustomerServiceApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8092"));
+		app.run(args);
 	}
 
 }
--- service-product/src/main/java/academy/digitallab/store/product/ServiceProductApplication.java
+++ service-product/src/main/java/academy/digitallab/store/product/ServiceProductApplication.java
@@ -3,10 +3,17 @@ package academy.digitallab.store.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ServiceProductApplication {
 
 	public static void main(String[] args) {
+		//SpringApplication app = new SpringApplication(ServiceProductApplication.class);
+		//app.setDefaultProperties(Collections
+		//		.singletonMap("server.port", "8091"));
+		//app.run(args);
+
 		SpringApplication.run(ServiceProductApplication.class, args);
 	}
 
